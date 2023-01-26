e9f8bf24b2c94d0eb661526e336ccbe3762a5e7f
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-26 16:47:55 +0300

    Cache

--- Security/src/main/java/com/works/restcontrollers/ProdcutRestController.java
+++ Security/src/main/java/com/works/restcontrollers/ProdcutRestController.java
@@ -5,12 +5,17 @@ import com.works.entities.Product;
 import com.works.repositories.NoteRepository;
 import com.works.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/product")
@@ -18,6 +23,7 @@ import java.util.Map;
 public class ProdcutRestController {
 
     final ProductRepository repository;
+    final CacheManager cacheManager;
 
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody Product note) {
@@ -25,15 +31,25 @@ public class ProdcutRestController {
         repository.save(note);
         hm.put("status", true);
         hm.put("result", note);
+        cacheManager.getCache("product").clear();
         return new ResponseEntity(hm, HttpStatus.OK);
     }
 
+
     @GetMapping("/list")
+    @Cacheable("product")
     public ResponseEntity list() {
         Map hm = new LinkedHashMap();
         hm.put("status", true);
         hm.put("result", repository.findAll());
         return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+
+    @Scheduled(fixedDelay = 9000, timeUnit = TimeUnit.MILLISECONDS)
+    public void timer() {
+        cacheManager.getCache("product").clear();
+        System.out.println("Timer Call");
     }
 
 }
